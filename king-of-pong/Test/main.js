da710062171ffcfedaf0730975353912ada22ad0
Jake Overall <joverall22@gmail.com>
2025-07-21 17:48:30 -0600

    Update main.js

--- king-of-pong/Test/main.js
+++ king-of-pong/Test/main.js
@@ -217,13 +217,17 @@ function fail(message) {
 
 // INTERCEPTOR
 window.log = []
+const queue = []
+let initialLoad = false
 let logBlock = '' // a join of the window log
-let fn = window.console.log
-window.console.log = function interceptor() {
+let fn = console.log
+console.log = function interceptor() {
   try {
-    fn(...arguments)
+    queue.push(arguments)
     window.log = [...window.log, ...arguments]
+    fn( ...arguments)
     logBlock = window.log.map(l => l === undefined ? 'undefined' : l).join(' ')
+
   } catch (e) {
     console.error(e)
   }
@@ -266,6 +270,10 @@ function runTests() {
 
   if (stats.passedTests == stats.totalTests) {
     console.log('✅', 'ALL TESTS PASSED!!!')
+  }
+  // CONSOLE LOGING THE QUEUE
+  if(queue.length){
+    queue.forEach(m => console.log(...m))
   }
 }
 
